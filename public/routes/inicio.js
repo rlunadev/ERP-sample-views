45bc4229f52eb4211610c2a0e0a78c149bf14212
ronald <ronaldwin79@hotmail.com>
2016-02-02 15:44:29 -0400

    modified home inicio and users

--- public/routes/inicio.js
+++ public/routes/inicio.js
@@ -27,5 +27,30 @@ angular.module('seedApp')
       templateUrl: path + '/inicio/paginas/usuarios.html',
       controller: 'InicioUsuariosController'
     });
+    $stateProvider.state('main.inicio.grupos', {
+      url: '/grupos',
+      templateUrl: path + '/inicio/paginas/grupos.html',
+      controller: 'InicioGruposController'
+    });
+    $stateProvider.state('main.inicio.parametros', {
+      url: '/parametros',
+      templateUrl: path + '/inicio/paginas/parametros.html',
+      controller: 'InicioParametrosController'
+    });
+    $stateProvider.state('main.inicio.configuracionContable', {
+      url: '/configuracionContable',
+      templateUrl: path + '/inicio/paginas/configuracionContable.html',
+      controller: 'InicioConfiguracionContableController'
+    });
+    $stateProvider.state('main.inicio.bitacora', {
+      url: '/bitacora',
+      templateUrl: path + '/inicio/paginas/bitacora.html',
+      controller: 'InicioBitacoraController'
+    });
+    $stateProvider.state('main.inicio.backup', {
+      url: '/backup',
+      templateUrl: path + '/inicio/paginas/backup.html',
+      controller: 'InicioBackupController'
+    });
   }
 ]);
